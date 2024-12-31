08dc98ff335335d8e50d8904b214f5a0df4a5e9b
Garrett Krohn <garrettkrohn@gmail.com>
2024-12-30 18:52:49 -0600

    more work on multiple components

---
+++ src/constants/about.md
@@ -0,0 +1,17 @@
+---
+title: "All about Ivan the Inspector!"
+---
+
+# How did Ivan the Inspector start?
+
+Ivan the Inspector is a 2 man operation consisting of Ivan and his producer and
+cameraman Chris. The two were coworkers in 2021 when Chris had an idea to make
+a kids show to rival the big names out there. At the time he had kids who were
+2 and 4, and he had lots of kids youtube on and decided that we could do it
+better. From the outset we wanted to be a channel that was more educational,
+less annoying for parents, and better video quality.
+
+# Is this a full time job?
+
+No, both Chris and Ivan do this part time. Chris is a pastor and freelance
+videographer for his day job, and Ivan is a Senior Software Engineer.
---
+++ src/constants/testimonialList.js
@@ -0,0 +1,33 @@
+const testimonialList = [
+  {
+    name: "Carol from TX, USA",
+    review:
+      "We found you on YouTube when looking for leaf blower videos for toddlers.  Sean has been hooked on Ivan ever since!  He even repeats whole episodes during his play time ('this is my backpack leaf blower! It needs some gas')",
+  },
+  {
+    name: "Harry from London, UK",
+    review: "My 2.5 year old loves your videos",
+  },
+  {
+    name: "Loretta from Australia",
+    review:
+      "Just wanted to thank you for your awesome videos!! My 16 month old son absolutely loves you (and your shows)! Asks to watch them every day and has a spy glass and vest like “I, I” which he says because of your opening song. Thanks again! I enjoy watching and learning also! :-)",
+  },
+  {
+    name: "Andrew",
+    review:
+      "Just wanted to let you know that my son absolutely loves your videos!  Keep up the great work!",
+  },
+  {
+    name: "Leah",
+    review:
+      "My husband and I love the format and appreciate all of the work you put in making your own graphics and songs and keeping things at a slow natural pace, similar to what they'd get if you were sitting with them in real life.  There are very few channels out there like yours - keep up the great work!",
+  },
+  {
+    name: "Ashely",
+    review:
+      "We are HUGE fans!! We have watched all of your videos at least half a million times!  Your haircut video helped my son get over his fear of haircuts.  You are greatly appreciated by littles and parents alike!",
+  },
+];
+
+export default testimonialList;
